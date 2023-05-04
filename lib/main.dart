ff89f0487d883e9c6cd299cfa9e597e339da5c67
Micky <96895869+Micky-12@users.noreply.github.com>
2023-05-04 15:49:26 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+        home: Scaffold(
+            appBar: AppBar(title: Text("Flutter"), centerTitle: true)));
+  }
+}
